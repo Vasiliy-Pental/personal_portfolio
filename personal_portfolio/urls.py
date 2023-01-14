632ca8604f15c8d4d891768b41c7095c195c857c
Bazill <vasia09@mail.ru>
2023-01-14 20:30:55 +0300

    new blog

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from portfolio import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -23,7 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home,name='home'),
     path('admin/', admin.site.urls),
-    #path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
